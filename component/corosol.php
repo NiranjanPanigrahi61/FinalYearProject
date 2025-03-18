33643c917ab3e18ceabb980945e2ea2b1ef31185
biswabishalsenapati21 <biswabishal00@gmail.com>
2025-03-18 20:11:08 +0530

    update image path

--- component/corosol.php
+++ component/corosol.php
@@ -6,14 +6,14 @@
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active position-relative">
-            <img src="../final_year_project/img/corosol1.jpg" class="d-block w-100" alt="Freshly Baked Bread">
+            <img src="../assets/corosol1.jpg" class="d-block w-100" alt="Freshly Baked Bread">
             <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100 w-100 position-absolute top-0 start-0 text-center">
                 <a href="h2.php" class="text-white text-decoration-none fs-2 fw-bold">Delicious Chocolate Cake</a>
                 <p class="fs-4 text-white fw-semibold">Rich, moist, and topped with creamy frosting – a perfect treat!</p>
             </div>
         </div>
         <div class="carousel-item position-relative">
-            <img src="../final_year_project/img/corosol2.jpg" class="d-block w-100" alt="Delicious Chocolate Cake">
+            <img src="../assets/corosol2.jpg" class="d-block w-100" alt="Delicious Chocolate Cake">
             <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100 w-100 position-absolute top-0 start-0 text-center">
                 <!-- <h5 class="fs-4">WE DELIVER ANYWHERE</h5> -->
                 <a href="h2.php" class="text-white text-decoration-none fs-2 fw-bold">Tasty Pastries</a>
@@ -21,7 +21,7 @@
             </div>
         </div>
         <div class="carousel-item position-relative">
-        <img src="../final_year_project/img/corosol3.jpg" class="d-block w-100" alt="Delicious Chocolate Cake">
+        <img src="../assets/corosol3.jpg" class="d-block w-100" alt="Delicious Chocolate Cake">
             <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100 w-100 position-absolute top-0 start-0 text-center">
             <a href="h2.php" class="text-white text-decoration-none fs-1 fw-bold">We Are Here To Bake You Happy!!</a>
             </div>
