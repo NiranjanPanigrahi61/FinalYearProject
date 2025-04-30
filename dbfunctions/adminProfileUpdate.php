85660e65e988e290f846bb9ff1aef23f81f2190f
NiranjanPanigrahi61 <156101527+NiranjanPanigrahi61@users.noreply.github.com>
2025-04-30 20:23:15 +0530

    some changes

--- dbfunctions/adminProfileUpdate.php
+++ dbfunctions/adminProfileUpdate.php
@@ -1,9 +1,36 @@
 <?php
+
+require_once __DIR__.'/dbconnect.php';
+require_once __DIR__.'/../aws/upload.php';
+
 $username=$_POST['username'];
 $email=$_POST['email'];
 $password=$_POST['password'];
-$image=$_FILES['adminimage'];
-
-echo $image['tmp_name'];
+// echo $image['tmp_name'];
+$response;
+if (isset($_FILES['adminimage']) && $_FILES['adminimage']['error'] === 0) {
+    $tmpName = $_FILES['adminimage']['tmp_name'];
+    $imgName = time() . "-" . $_FILES['adminimage']['name'];
+    require_once "../aws/upload.php";
+    $response = uploadtoS3("admin", $tmpName, $imgName);
+}
+if ($response) {
+    try {
+        $qry="UPDATE admininfo SET username=?, email=?, password=?, adminimage=?";
+        $stmt=$conn->prepare($qry);
+        $stmt->bind_param("ssss",$username,$email,$password,$response);
+        if($stmt->execute()){
+            echo true;
+        }else{
+            echo false;
+        }
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }finally{
+        $conn->close();
+    }
+} else {
+    echo false;
+}
 
 ?>
